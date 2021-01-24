67f4e114f967088044267e9f8a60b8ea183513e4
Marc <marc.ferraro1@gmail.com>
2021-01-24 14:10:21 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+
+  get '/', to: 'products#index'
+
+  post '/', to: 'products#add'
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
 end
